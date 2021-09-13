b69dc815d12827678215f4f187a51cbd8c43d683
Matthew Arteaga <matthew.arteaga@daysmart.com>
2021-09-13 16:40:37 -0400

    PLATFORM-1 Added an app-params provider as well as secret decryption in the base provider.

---
+++ packages/providers/app-params/lib/app-param-provider.ts
@@ -0,0 +1,29 @@
+import { SSM } from 'aws-sdk';
+import { Provider } from "@daysmart/frankenstack-base-provider/assets/Provider";
+
+export class AppParamsProvider extends Provider {
+    async provisionComponent() {
+        let client = new SSM();
+        
+        try {
+            const paramName = `${this.environment}-${this.componentName}`;
+            const resp = await client.putParameter({
+                Name: paramName,
+                Value: JSON.stringify(Object.assign({}, ...this.inputs.map(input => {return {[input.Key]: input.Value}}))),
+                Type: 'SecureString',
+                Overwrite: true
+            }).promise();
+    
+            console.log(resp);
+
+            this.outputs.push({
+                Key: 'APP_PARAMETERS',
+                Value: paramName
+            });
+
+            this.result = true;
+        } catch(err) {
+            console.error(err);
+        }
+    }
+}
--- packages/providers/base/assets/Handler.ts
+++ packages/providers/base/assets/Handler.ts
@@ -2,6 +2,7 @@ import { Provider} from "./Provider";
 
 export class Handler<T extends Provider> {
     async run(provider: T): Promise<void> {
+        await provider.decryptInputs();
         await provider.provisionComponent();
         await provider.sendResponse();
     }
