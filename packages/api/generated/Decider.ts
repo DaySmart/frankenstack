708a15ce56f19ea39c0bdbf12c8f1103d96c2e78
Shereef Marzouk <shereef.marzouk@daysmart.com>
2025-11-04 19:59:42 -0400

    fix: update Decider class to use Query[] for filters and improve load method parameters

--- packages/api/generated/Decider.ts
+++ packages/api/generated/Decider.ts
@@ -2,24 +2,31 @@ import { Repository } from "../plugin/repository/repository";
 import { Observation2 } from "./Observation2";
 import { IEntityObservation } from "./Entities/IEntityObservation";
 import { Handler } from "./Handler";
+import { Query } from "o18k-ts-aws";
 
 export class Decider<T extends IEntityObservation> {
   constructor(
     public traceid: string,
-    public filters: any,
+    // queries describing prior observations needed to make a decision
+    public filters: Query[],
+    // the current/root observation driving these dependent lookups
+    public observation: Observation2<IEntityObservation>,
     public handler: Handler<T>,
     public log: any,
     public repository: Repository
   ) {}
 
   public async execute(): Promise<Observation2<T>[]> {
-    const dependentObservations = await this.repository.load(this.filters);
+    // Load dependent observations based on provided queries & current observation context
+    const dependentObservations = await this.repository.load(
+      this.observation,
+      this.filters
+    );
     const decisions = this.handler.run(
       dependentObservations,
       this.traceid,
       this.log
     );
-    //compare
     return decisions;
   }
 }
